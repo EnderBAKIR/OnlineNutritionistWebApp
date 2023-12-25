7d9b99224ff6f1370e3f9268265b357de1d13ccb
Ender Bakır <enderyx_123@hotmail.com>
2023-12-26 02:58:58 +0300

    Yorum Atma Ve Listeleme
    Bloglara yorum atma ve listeleme işlemleri için CommentController ve Commenlist Componenti oluşturuldu. Core katmanında Modellerde düzenlenmeler yapıldı. Repository katmanında Configuration Classları yeniden yapılandırıldı. Yorumlar için ayrı bir CommentRepository Class'ı ve CommentService Class'ı tanımlandı.Controllerların Viewleri düzenlendi. Buglar fixlendi.

--- OnlineNutritionistWebApp/OnlineNutritionistProject/Controllers/BlogsController.cs
+++ OnlineNutritionistWebApp/OnlineNutritionistProject/Controllers/BlogsController.cs
@@ -3,32 +3,40 @@ using CoreLayer.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
 
 namespace OnlineNutritionistProject.Controllers
 {
-    [AllowAnonymous]
-    public class BlogsController : Controller
-    {
-        private readonly IBlogService _blogService;
+	[AllowAnonymous]
+	public class BlogsController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly IBlogService _blogService;
 
-        public BlogsController(IBlogService blogService)
+        public BlogsController(UserManager<AppUser> userManager, IBlogService blogService)
         {
+            _userManager = userManager;
             _blogService = blogService;
         }
 
         public async Task<IActionResult> Index()
-        {
-            return View(await _blogService.GetAllAsync());
-        }
+		{
+			return View(await _blogService.GetAllAsync());
+		}
 
+	
+		[HttpGet]
+		public async Task<IActionResult> BlogsDetails(int id)
 
-        [HttpGet]
-        public async Task<IActionResult> BlogsDetails(int id)
+		{
+			var value = await _userManager.FindByNameAsync(User.Identity.Name);
+			ViewBag.AppuserId = value.Id;
+			ViewBag.BlogId = id;
+			var values = await _blogService.GetByIdAsync(id);
+			return View(values);
 
-        {
-            var values = await _blogService.GetByIdAsync(id);
-            return View(values);
+		}
 
-        }
-    }
+				
+	}
 }
---
+++ OnlineNutritionistWebApp/RepositoryLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,25 @@
+﻿using CoreLayer.Models;
+using CoreLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repositories
+{
+    public class CommentRepository : GenericRepository<Comment>, ICommentRepository
+    {
+        public CommentRepository(Context dbContext) : base(dbContext)
+        {
+        }
+
+        public List<Comment> GetCommentWithBlogs(int id)
+        {
+            
+            return _dbContext.Comments.Where(x => x.BlogID == id).Include(x => x.AppUser).ToList();
+        }
+    }
+}
