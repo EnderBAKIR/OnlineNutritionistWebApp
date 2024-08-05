c59840b85a21325643b1ace53e71c4312b0d061a
Ender Bakır <Enderyx_123@hotmail.com>
2024-08-05 19:45:38 +0300

    New Feature: We can see the blogs of the nutritionist we want on the nutritionist page.

---
+++ CoreLayer/Repositories/IAppuserRepository.cs
@@ -0,0 +1,20 @@
+﻿using CoreLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLayer.Repositories
+{
+    public interface  IAppuserRepository: IGenericRepository<AppUser>
+    {
+        public Task<List<AppUser>> GetNutritionWithService();
+
+        public Task<List<AppUser>> GetNutritionists();
+
+        public Task<AppUser> GetNutritionistById(int id);
+
+
+    }
+}
---
+++ RepositoryLayer/Repositories/AppuserRepository.cs
@@ -0,0 +1,35 @@
+﻿using CoreLayer.Models;
+using CoreLayer.Repositories;
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Repositories
+{
+    public class AppuserRepository : GenericRepository<AppUser>, IAppuserRepository
+    {
+        public AppuserRepository(Context dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<List<AppUser>> GetNutritionists()
+        {
+            return await _dbContext.AppUsers.Include(x=>x.Consultancy).Where(u => u.AppNutriId != null).ToListAsync();
+        }
+
+        public async Task<AppUser> GetNutritionistById(int id)
+        {
+            return await _dbContext.AppUsers.Include(x=> x.Blogs).Where(u=>u.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<AppUser>> GetNutritionWithService()
+        {
+            
+            return await _dbContext.AppUsers.Include(c=> c.Blogs).ToListAsync();
+        }
+    }
+}
