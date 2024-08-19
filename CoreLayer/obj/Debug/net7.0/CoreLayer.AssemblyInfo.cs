dc795dc691609e8a17ed7ed6be881b4418be6bdf
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-08-19 21:59:51 +0300

    The dietitian panel has been changed and updates have been made.

--- CoreLayer/obj/Debug/net7.0/CoreLayer.AssemblyInfo.cs
+++ CoreLayer/obj/Debug/net7.0/CoreLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eed1dd9888c86bac1063fb3a68924387644c1ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a69fde1addc70beafd612692b73b6abb8212565")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
