b5112f6cbb72fa5e265b42482b873998202faf15
Ender Bakır <Enderyx_123@hotmail.com>
2024-09-10 18:29:15 +0300

    Member Area => Added BasketController / Index

---
+++ OnlineNutritionistProject/Areas/Member/Controllers/BasketController.cs
@@ -0,0 +1,31 @@
+﻿using CoreLayer.Models;
+using CoreLayer.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineNutritionistProject.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class BasketController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IBasketService _basketService;
+        private readonly IPackageService _packageService;
+
+        public BasketController(UserManager<AppUser> userManager, IBasketService basketService, IPackageService packageService)
+        {
+            _userManager = userManager;
+            _basketService = basketService;
+            _packageService = packageService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var basketItems = await _basketService.GetBasketByAppUserIdAsync(user.Id);
+            var package = await _packageService.GetByIdAsync(basketItems.Select(x => x.PackageIdentity).First());
+            ViewBag.packageName = package.Title;
+            return View(basketItems);
+        }
+    }
+}
--- OnlineNutritionistProject/obj/Debug/net7.0/OnlineNutritionistProject.AssemblyInfo.cs
+++ OnlineNutritionistProject/obj/Debug/net7.0/OnlineNutritionistProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineNutritionistProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8df8895011f41dfdffab10d15094cca7e045acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59dda3b8cb3fee8236955742c17a9b0931bb16c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineNutritionistProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineNutritionistProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
